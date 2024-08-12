090eda90c34a12a3cf55ed3b02984ebb97cbc108
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-08-12 11:14:34 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import dotenv from 'dotenv';
-import contactsRouter from './routers/contacts.js'
+import router from './routers/contacts.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 dotenv.config();
-const PORT = process.env.PORT || 3001;
+const PORT = Number(env('PORT'))
  
 const setupServer = () => {
   const app = express();
@@ -19,7 +19,7 @@ const setupServer = () => {
     }),
   );
   app.use(express.json());
-app.use(contactsRouter);
+app.use(router);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
   app.listen(PORT, () => {
